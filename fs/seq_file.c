13a15a7734921cb254cb4ff9d0223cc03f503e75
Sultanxda <sultanxda@gmail.com>
2015-03-26 23:27:58 -0700

    kernel: Sync with CAF LA.BF.1.1.1_rb1.17

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -13,8 +13,6 @@
 #include <asm/uaccess.h>
 #include <asm/page.h>
 
-#include <linux/mm.h>
-#include <linux/vmalloc.h>
 
 /*
  * seq_files have a buffer which can may overflow. When this happens a larger
@@ -133,12 +131,8 @@ static int traverse(struct seq_file *m, loff_t offset)
 
 Eoverflow:
 	m->op->stop(m, p);
-	is_vmalloc_addr(m->buf) ? vfree(m->buf) : kfree(m->buf);
-	m->size <<= 1;
-	if (m->size <= (2 * PAGE_SIZE))
-		m->buf = kmalloc(m->size, GFP_KERNEL);
-	else
-		m->buf = vmalloc(m->size);
+	kfree(m->buf);
+	m->buf = kmalloc(m->size <<= 1, GFP_KERNEL);
 	return !m->buf ? -ENOMEM : -EAGAIN;
 }
 
@@ -233,13 +227,8 @@ ssize_t seq_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
 		if (m->count < m->size)
 			goto Fill;
 		m->op->stop(m, p);
-		is_vmalloc_addr(m->buf) ? vfree(m->buf) : kfree(m->buf);
-		m->size <<= 1;
-		if (m->size > 2 * PAGE_SIZE)
-			m->buf = vmalloc(m->size);
-		else
-			m->buf = kmalloc(m->size, GFP_KERNEL);
-
+		kfree(m->buf);
+		m->buf = kmalloc(m->size <<= 1, GFP_KERNEL);
 		if (!m->buf)
 			goto Enomem;
 		m->count = 0;
@@ -354,7 +343,7 @@ EXPORT_SYMBOL(seq_lseek);
 int seq_release(struct inode *inode, struct file *file)
 {
 	struct seq_file *m = file->private_data;
-	is_vmalloc_addr(m->buf) ? vfree(m->buf) : kfree(m->buf);
+	kfree(m->buf);
 	kfree(m);
 	return 0;
 }
--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -280,9 +280,6 @@ static unsigned long vmap_area_pcpu_hole;
 #define VMALLOC_TO_BIT(addr)	((addr - PAGE_OFFSET) >> PAGE_SHIFT)
 #define BIT_TO_VMALLOC(i)	(PAGE_OFFSET + i * PAGE_SIZE)
 
-unsigned long total_vmalloc_size = 0;
-unsigned long vmalloc_reserved = 0;
-
 DECLARE_BITMAP(possible_areas, VMALLOC_BITMAP_SIZE);
 
 void mark_vmalloc_reserved_area(void *x, unsigned long size)
@@ -290,7 +287,6 @@ void mark_vmalloc_reserved_area(void *x, unsigned long size)
 	unsigned long addr = (unsigned long)x;
 
 	bitmap_set(possible_areas, VMALLOC_TO_BIT(addr), size >> PAGE_SHIFT);
-	vmalloc_reserved += size;
 }
 
 int is_vmalloc_addr(const void *x)
@@ -305,12 +301,6 @@ int is_vmalloc_addr(const void *x)
 
 	return 1;
 }
-
-static void calc_total_vmalloc_size(void)
-{
-        total_vmalloc_size = VMALLOC_END - POSSIBLE_VMALLOC_START -
-                vmalloc_reserved;
-}
 #else
 int is_vmalloc_addr(const void *x)
 {
@@ -318,8 +308,6 @@ int is_vmalloc_addr(const void *x)
 
 	return addr >= VMALLOC_START && addr < VMALLOC_END;
 }
-
-static void calc_total_vmalloc_size(void) { }
 #endif
 EXPORT_SYMBOL(is_vmalloc_addr);
 
@@ -1271,7 +1259,6 @@ void __init vmalloc_init(void)
 
 	vmap_area_pcpu_hole = VMALLOC_END;
 
-	calc_total_vmalloc_size();
 	vmap_initialized = true;
 }
 
